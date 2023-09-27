8cd7c48ba5bd8712ab48451c39ffaec90471441d
Wen Barbosa <wenblack012@gmail.com>
2023-09-27 11:29:37 -0300

    🎨 Created theme(typography/colors) of project 🎨

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="flex justify-center w-screen h-44 fixed top-0 bg-hero-pattern">
+      <h1 className="text-gray-100 ">Lista de Compras</h1>
+    </header>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'purple-light': '#A881E6',
+      'purple': '#7450AC',
+      'purple-dark': '#523480',
+      'gray': {
+        '100': '#FBF9FE',
+        '200': '#AFABB6',
+        '300': '#252529',
+        '400': '#17171A',
+        '500': '#111112',
+        '600': '#0C0C0D'
+      },
+      'success-light': '#4E995E',
+      'success': '#2F723D',
+      'pink': ' #DB5BBF',
+      'pink-dark': '#251622',
+      'orange': '#E07B67',
+      'orange-black': '#261A17',
+      'yellow': ' #BB9F3A',
+      'yellow-black': '#211E12',
+      'green': ' #8CAD51',
+      'green-dark': '#1C2015',
+      'blue': '#7B94CB',
+      'blue-dark': '#1A1D23'
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./src/assets/HeroImage.png')",
+      }
+    },
   },
   plugins: [],
 }
-

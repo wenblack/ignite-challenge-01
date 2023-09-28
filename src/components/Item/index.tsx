60d4ca157b5dfdd91c98eb24111e9df56a3f8092
Wen Barbosa <wenblack012@gmail.com>
2023-09-28 10:33:12 -0300

    Created logic to change style from state

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import checkedIcon from '../../assets/checkbox.svg'
 import squareIcon from '../../assets/uncheckedbox.svg'
 import fruit from '../../assets/fruit.svg'
-import milk from '../../assets/milk.svg'
 import { Tag } from "../Tag"
 
 interface ItemProps {
@@ -15,39 +14,27 @@ interface ItemProps {
 
 export function Item({ name, amount, categorie, unity }: ItemProps) {
   const [completed, setIsCompleted] = useState(false)
+  const [opacity, setOpacity] = useState(100)
 
   function handleToggleChangeItem() {
     if (completed) {
       setIsCompleted(false)
+      setOpacity(100)
     } else {
       setIsCompleted(true)
+      setOpacity(80)
     }
   }
 
-  if (completed) {
-    return (
-      <li onClick={handleToggleChangeItem} className="hover:cursor-pointer opacity-80 rounded-xl  p-4 bg-gray-400 flex items-center justify-between">
-        <span className="flex gap-4">
-          <img src={completed ? checkedIcon : squareIcon} alt="" />
-          <p className=" flex-col text-gray-200">
-            <h2 className="line-through ">{name}</h2>
-            {`${amount} ${unity}`}
-          </p>
-        </span>
-        <Tag
-          categorieName={categorie}
-          imgUlr={
-            categorie === 'fruta' ? fruit : milk}
-        />
-      </li>
-    )
-  }
   return (
-    <li onClick={handleToggleChangeItem} className="hover:cursor-pointer rounded-xl  p-4 bg-gray-400 flex items-center justify-between">
+    <li onClick={handleToggleChangeItem} className={`hover:cursor-pointer  opacity-${opacity} rounded-xl  p-4 bg-gray-400 flex items-center justify-between`}>
       <span className="flex gap-4">
         <img src={completed ? checkedIcon : squareIcon} alt="" />
         <p className=" flex-col text-gray-200">
-          <h2>{name}</h2>
+          {
+            completed ? <h2 className="line-through ">{name}</h2>
+              : <h2>{name}</h2>
+          }
           {`${amount} ${unity}`}
         </p>
       </span>
